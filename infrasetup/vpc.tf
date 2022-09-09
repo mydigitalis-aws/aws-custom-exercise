e1d27cd9b863b81fedaa3f8b3239b2656f9a0268
unknown <raghu.polavarapu@gmail.com>
2022-09-09 17:19:38 +0530

    Plan updates

--- infrasetup/vpc.tf
+++ infrasetup/vpc.tf
@@ -15,7 +15,6 @@ resource "aws_subnet" "digitalis-subnet-public-1" {
     vpc_id = "${aws_vpc.digitalis-vpc.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true" //it makes this a public subnet where deployed ec2 instances get a public IP
-    #availability_zone = "us-east-1a"
     availability_zone = "${var.AWS_AVAILABILITY_ZONE}"
 
     tags = {
@@ -28,7 +27,7 @@ resource "aws_subnet" "digitalis-subnet-private-1" {
     vpc_id = "${aws_vpc.digitalis-vpc.id}"
     cidr_block = "10.0.10.0/24"
     map_public_ip_on_launch = "false" //keeps this private so that ec2 instances deployed here get a private IP
-    #availability_zone = "us-east-1a"
+   
     availability_zone = "${var.AWS_AVAILABILITY_ZONE}"
 
     tags = {
@@ -41,7 +40,6 @@ resource "aws_subnet" "digitalis-subnet-workload-1" {
     vpc_id = "${aws_vpc.digitalis-vpc.id}"
     cidr_block = "10.0.20.0/24"
     map_public_ip_on_launch = "false" //keeps this private so that ec2 instances deployed here get a private IP
-    #availability_zone = "us-east-1a"
     availability_zone = "${var.AWS_AVAILABILITY_ZONE}"
 
     tags = {
